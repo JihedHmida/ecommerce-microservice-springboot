4e4b62b2ee32edb26057d085442a3987b8578cb4
Jihed Hmida <jihed.hmida@gmail.com>
2024-08-29 00:59:53 +0100

    add product-service

---
+++ microservices/product/src/test/java/com/jh/ecommerce/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.ecommerce.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
